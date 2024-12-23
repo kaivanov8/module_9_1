daef1b9a0c5fdbdeef3eec69319175be054781bb
kaivanov8 <kosiv6799@gmail.com>
2024-12-23 19:18:35 +0700

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,10 @@
+# введение в функциональное программирование
+
+def apply_all_func(int_list, *functions):
+    result = {}
+    for function in functions:
+        result.update({function.__name__:function(int_list)})
+    return result
+
+print(apply_all_func([6,20,15,9],max, min))
+print(apply_all_func([6,20,15,9],len,sum,sorted))
